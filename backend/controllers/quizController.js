d853e9b2362131901fd9e48763b8e5a8f9eeb19b
tbence12 <38697221+tbence12@users.noreply.github.com>
2023-05-09 20:01:32 +0200

    BE05: Extend request functions and CORS (#12)

--- backend/controllers/quizController.js
+++ backend/controllers/quizController.js
@@ -1,4 +1,6 @@
+import mongoose from "mongoose";
 import QuizModel from '../models/quizModel';
+import ResultModel from '../models/resultModel';
 
 export const addNewQuiz = (req, res) => {
   let newQuiz = new QuizModel(req.body);
@@ -38,8 +40,53 @@ export const updateQuiz = async (req, res) => {
 export const deleteQuiz = async (req, res) => {
   try {
     const quizId = req.params.quizId;
-    await QuizModel.deleteOne({_id: quizId});
+    await QuizModel.findOneAndUpdate({_id: quizId}, {isDeleted: true});
     res.json({ message: `Successfully deleted quiz with id: ${quizId}.`});
+  } catch (error) {
+    res.status(500).json({message: error.message});
+  }
+}
+
+export const getUnfilledQuizzes = async (req, res) => {
+  try {
+    const userId = req.params.userId;
+
+    const results = await ResultModel.find({ userId: userId });
+    const quizIds = results.map(result => result.quizId);
+    const quizzes = await QuizModel.find({ _id: { $nin: quizIds }, isDeleted: false });
+    
+    res.json(quizzes);
+  } catch (error) {
+    res.status(500).json({message: error.message});
+  }
+}
+
+export const getQuizWithQuestions = async (req, res) => {
+  try {
+    const userId = req.params.userId;
+    const quizId = req.params.quizId;
+    
+    const results = await ResultModel.find({ userId: userId });
+    const quizIds = results.map(result => result.quizId);
+
+    if(quizIds.toString().includes(quizId)) {
+      res.json([]);
+    } else {
+      const quizObjectId = new mongoose.Types.ObjectId(quizId);
+      const quizzes = await QuizModel.aggregate([
+        { $match:{_id: quizObjectId, isDeleted: false} },
+        {
+          $lookup: {
+            from: "questions",
+            localField: "questionIds",
+            foreignField: "_id",
+            as: "questions"
+          }
+        },
+      ]).exec();
+    
+      res.json(quizzes);
+    }
   } catch (error) {
     res.status(500).json({message: error.message});
   }
--- backend/routes/quizRoutes.js
+++ backend/routes/quizRoutes.js
@@ -1,14 +1,20 @@
-import { addNewQuiz, getQuizzes, getQuizById, updateQuiz, deleteQuiz } from "../controllers/quizController";
+import { addNewQuiz, getQuizzes, getQuizById, updateQuiz, deleteQuiz, getUnfilledQuizzes, getQuizWithQuestions } from "../controllers/quizController";
 
 const quizRoutes = (app) => {
   app.route('/quizzes')
     .get(getQuizzes)
     .post(addNewQuiz);
-  
-  app.route('/quiz/:quizId')
+    
+    app.route('/quiz/:quizId')
     .get(getQuizById)
     .put(updateQuiz)
     .delete(deleteQuiz);
-}
+
+    app.route('/quizzes/unfilled/:userId')
+      .get(getUnfilledQuizzes);
+    
+    app.route('/quiz/questions/:userId/:quizId')
+      .get(getQuizWithQuestions);
+  }
 
 export default quizRoutes;
